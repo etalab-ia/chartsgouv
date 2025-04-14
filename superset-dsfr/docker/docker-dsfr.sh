ce4f41068f0093286d3d158670aeebc8e4ca49f1
Quentin Leroy <qleroy@users.noreply.github.com>
2025-04-14 15:40:02 +0200

    restore latest dsfr theme and guide (#24)

---
+++ superset-dsfr/docker/docker-dsfr.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+for theme_filename in $(find /app/superset/static/assets -name "theme*.css"); do
+    sed \
+      -e "s/#20a7c9/#000091/g" \
+      -e "s/#45bed6/#000091/g" \
+      -e "s/#1985a0/#000091/g" \
+      "$theme_filename" > temp.css && mv temp.css "$theme_filename"
+done
+
+pybabel compile -d superset/translations || true
+
+cp /app/superset/templates_overrides/superset/{base,basic,public_welcome}.html /app/superset/templates/superset/
+cp /app/superset/templates_overrides/tail_js_custom_extra.html /app/superset/templates/tail_js_custom_extra.html
+cp /app/superset/static/assets/local/404.html /app/superset/static/assets/404.html
+cp /app/superset/static/assets/local/500.html /app/superset/static/assets/500.html

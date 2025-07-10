7b014783b230120d0edf5e692bb7baebbb6280e1
Quentin Leroy <quentin.n.leroy@gmail.com>
2025-07-10 09:20:10 +0000

    fix docker-dsfr.sh

--- superset/docker/docker-dsfr.sh
+++ superset/docker/docker-dsfr.sh
@@ -10,7 +10,7 @@ done
 
 pybabel compile -d superset/translations || true
 
-cp /app/superset/templates_overrides/superset/{base,basic,public_welcome}.html /app/superset/templates/superset/
+cp /app/superset/templates_overrides/superset/public_welcome.html /app/superset/templates/superset/
 cp /app/superset/templates_overrides/tail_js_custom_extra.html /app/superset/templates/tail_js_custom_extra.html
 cp /app/superset/static/assets/local/404.html /app/superset/static/assets/404.html
 cp /app/superset/static/assets/local/500.html /app/superset/static/assets/500.html

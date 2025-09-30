26677661d7278ba8aa8609c94295c7dcc8dba804
ytihianine <ytihianine.pro@gmail.com>
2025-09-30 12:50:07 +0200

    fix(build): update Dockerfile (#90)
    * add back basic and base templates for previous version compatibility
    
    * merge translations files into a single file
    
    * add logs and making command more generic
    
    * add coment in docker compose file
    
    * update Dockerfile to match new repo organisation

--- superset-dsfr/docker/docker-dsfr.sh
+++ superset-dsfr/docker/docker-dsfr.sh
@@ -8,10 +8,23 @@ for theme_filename in $(find /app/superset/static/assets -name "theme*.css"); do
       "$theme_filename" > temp.css && mv temp.css "$theme_filename"
 done
 
-pybabel compile -d superset/translations || true
+# Compile translations if folder exists
+if [ -d /app/superset/translations ]; then
+  echo "Compiling translations..."
+  pybabel compile -d superset/translations || true
+  echo "Translations compiled."
+fi
 
-cp /app/superset/templates_overrides/superset/public_welcome.html /app/superset/templates/superset/
-cp /app/superset/templates_overrides/head_custom_extra.html /app/superset/templates/head_custom_extra.html
-cp /app/superset/templates_overrides/tail_js_custom_extra.html /app/superset/templates/tail_js_custom_extra.html
-cp /app/superset/static/assets/local/404.html /app/superset/static/assets/404.html
-cp /app/superset/static/assets/local/500.html /app/superset/static/assets/500.html
+# Copy templates overrides if folder exists
+if [ -d /app/superset/templates_overrides ]; then
+  echo "Copying template overrides..."
+  cp -r /app/superset/templates_overrides/* /app/superset/templates/
+  echo "Template overrides copied."
+fi
+
+# Copy local assets if folder exists
+if [ -d /app/superset/static/assets/local ]; then
+  echo "Copying local assets..."
+  cp -r /app/superset/static/assets/local/* /app/superset/static/assets/
+  echo "Local assets copied."
+fi
